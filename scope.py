a785351293f8085d26423b7e493c8d5035559a39
chris cale <chris.cale@outlook.com>
2021-12-26 17:37:26 -0600

    added example of global verse local scope as demonstrated with functions

---
+++ scope.py
@@ -0,0 +1,29 @@
+#For Practice to show an example of how scope works in Python.
+
+foo = 50
+bar = 90
+
+def local_scope_test():
+    foo = 2
+    bar = 7
+    
+    print("Local Scope Test - defined variables within the function:")
+    print('Local  foo = ', foo, '\tLocal  bar = ', bar, '')
+    print_global_values()
+    
+def global_scope_test():
+    global foo
+    foo = 80
+    bar = 7
+    
+    print("Global Scope Test - referenced global foo and defined bar locally:")
+    print('Local  foo = ', foo, '\tLocal  bar = ', bar, '')
+    print_global_values()
+    
+def print_global_values():
+    print('Global foo = ', foo, '\tGlobal bar = ', bar, '\n')
+    
+#run each test
+print('Global values before testing:\tfoo = ', foo, '\tGlobal bar = ', bar, '\n')
+local_scope_test()
+global_scope_test()
