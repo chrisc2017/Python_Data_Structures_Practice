dc12e4b44a84b37fbe93c1f3261eaa54c78a0bd6
chris cale <chris.cale@outlook.com>
2021-12-26 19:38:01 -0600

    added script to showcase how objects can be compared

---
+++ object_comparison.py
@@ -0,0 +1,10 @@
+#For Practice to show an example of how scope works in Python.
+
+def compare_object_values():
+    pass
+
+def compare_if_same_object():
+    pass
+
+def compare_if_object_same_type():
+    pass
