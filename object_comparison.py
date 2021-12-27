013599015a0e9c1b196999f0237c8355b6f453a3
chris cale <chris.cale@outlook.com>
2021-12-26 19:47:03 -0600

    added comparisons and output formatting

--- object_comparison.py
+++ object_comparison.py
@@ -1,10 +1,19 @@
 #For Practice to show an example of how scope works in Python.
 
-def compare_object_values():
-    pass
+foo = 'apples'
+bar = 'worms'
 
-def compare_if_same_object():
-    pass
+def compare_object_values(foo, bar):
+    print('Do foo and bar have the same value?\t', foo==bar)
+
+def compare_if_same_object(foo, bar):
+    print('Are foo and bar the same object?\t', foo is bar)
 
 def compare_if_object_same_type():
-    pass
+    print('Are foo and bar of the same type?\t', type(foo) is type(bar))
+    
+
+print('foo = ', foo, '\tbar = ', bar, '\n')
+compare_object_values(foo, bar)
+compare_if_same_object(foo, bar)
+compare_if_object_same_type()
